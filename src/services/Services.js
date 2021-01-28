0c4bec663278d9a50c2a694ee9f37552876481d1
Korn_SAMART <kasettakorn1997@gmail.com>
2021-01-28 11:15:43 +0700

    Add exactIntegrate service

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,4 +1,6 @@
 import { compile, derivative } from 'mathjs';
+var Algebrite = require('algebrite');
+
 const func = (fx, X) => {
     var expr = compile(fx); // f(x)
     let scope = { x: parseFloat(X) }; //f(x) ; x=input
@@ -12,5 +14,9 @@ const funcDiff = (X) => {
 const error = (xnew, xold) => {
     return Math.abs((xnew - xold) / xnew);
 }
+const exactIntegrate = (a, b) => {
+    var expr = compile(Algebrite.integral(Algebrite.evaluate(this.state.fx)).toString())
+    return expr.evaluate({x:b}) - expr.evaluate({x:a})
 
-export { func, funcDiff, error };
+}
+export { func, funcDiff, error, exactIntegrate };
